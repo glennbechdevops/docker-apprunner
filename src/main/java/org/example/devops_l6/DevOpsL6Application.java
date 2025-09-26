ab2a90600e28dd94523c0f93c29e60e852ad0dcc
amalie246 <amni002@egms.no>
2025-09-26 15:39:47 +0200

    init commit

---
+++ src/main/java/org/example/devops_l6/DevOpsL6Application.java
@@ -0,0 +1,13 @@
+package org.example.devops_l6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsL6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsL6Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/devops_l6/DevOpsL6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devops_l6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsL6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
